3eb24d78bda63b33cba9826889bef280c697d521
HelloOS_in_luogu <109774335+HelloOSMe@users.noreply.github.com>
2022-11-20 14:30:39 +0800

    Update crawler.py
    Signed-off-by: HelloOS_in_luogu <109774335+HelloOSMe@users.noreply.github.com>

--- crawler.py
+++ crawler.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import *
 import time
+import sys
 #import lxml
 head = {
     "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 10.0; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11"
 }
 #url="https://helloos.wikidot.com/" 
 #输入您的URL
-url=input()
+url=sys.argv[1]
 res=requests.get(url, headers=head)
 res.encoding=res.apparent_encoding
 print(res)
